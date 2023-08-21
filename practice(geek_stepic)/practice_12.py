1eaceb319dd4a381525e01fb4d47126dc40f783a
Kes <kostilio911@gmail.com>
2023-08-21 22:37:10 +0400

    done practice 12

---
+++ practice(geek_stepic)/practice_12.py
@@ -0,0 +1,317 @@
+"""
+Задание №1
+Создайте класс-функцию, который считает факториал числа при
+вызове экземпляра.
+Экземпляр должен запоминать последние k значений.
+Параметр k передаётся при создании экземпляра.
+Добавьте метод для просмотра ранее вызываемых значений и
+их факториалов.
+"""
+
+from math import factorial
+
+
+class Factorial:
+
+    def __init__(self, count: int = 1) -> None:
+        self.history = []
+        self.count = count
+
+    def __call__(self, n: int = 1) -> list[int]:
+        res = factorial(n)
+        self.history.append({n: res})
+        self.history = self.history[-self.count:]
+        return res
+
+    def get_history(self):
+        return self.history
+
+
+if __name__ == '__main__':
+    f = Factorial(3)
+    for i in range(1, 11):
+        print(f'{i}! = {f(i)}')
+
+    print(f.get_history())
+
+
+"""
+Задание №2
+Доработаем задачу 1.
+Создайте менеджер контекста, который при выходе
+сохраняет значения в JSON файл.
+"""
+
+
+from math import factorial
+import json
+
+
+class Factorial:
+
+    def __init__(self, count: int = 1) -> None:
+        self.history = []
+        self.count = count
+
+    def __call__(self, n: int = 1) -> list[int]:
+        res = factorial(n)
+        self.history.append({n: res})
+        self.history = self.history[-self.count:]
+        return res
+
+    def get_history(self):
+        return self.history
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        with open('factorial.json', 'w', encoding="UTF-8") as js_f:
+            json.dump(self.history, js_f)
+
+
+if __name__ == '__main__':
+    f = Factorial(6)
+    with f as js_f:
+        for i in range(1, 7):
+            print(f'{i}! = {f(i)}')
+
+
+"""
+Задание №3
+Создайте класс-генератор.
+Экземпляр класса должен генерировать факториал числа в
+диапазоне от start до stop с шагом step.
+Если переданы два параметра, считаем step=1.
+Если передан один параметр, также считаем start=1.
+"""
+
+class Factorial:
+
+    def __init__(self, *args) -> None:
+        match args:
+            case (stop, ):
+                self.stop = stop
+                self.start = 1
+                self.step = 1
+            case (start, stop, ):
+                self.start = start
+                self.stop = stop
+                self.step = 1
+            case (start, stop, step):
+                self.start = start
+                self.stop = stop
+                self.step = step
+        self.res = 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while self.start <= self.stop:
+            self.res *= self.start
+            self.start += self.step
+            return self.res
+        raise StopIteration
+
+
+if __name__ == '__main__':
+    f = Factorial(1, 5, 1)
+    print(list(f))
+    f = Factorial(9)
+    print(list(f))
+
+
+"""
+Задание №4
+Доработайте класс прямоугольник из прошлых семинаров.
+Добавьте возможность изменять длину и ширину
+прямоугольника и встройте контроль недопустимых значений
+(отрицательных).
+Используйте декораторы свойств.
+"""
+
+class Rectangle:
+    """
+    Класс "Прямоугольник" для выполнения действий с прямоугольниками,
+    позволяет сравнивать прямоугольники по площади,
+    получить площадь и периметр прямоугольников
+    складывать и вычитать прямоугольники
+    """
+
+    # __slots__ = ('_width', '_length')
+
+    def __init__(self, side_a, side_b=0):
+        self._width = side_a
+        if side_b == 0:
+            side_b = side_a
+        self._length = side_b
+
+    @property
+    def width(self):
+        return self._width
+
+    @property
+    def length(self):
+        return self._length
+
+    @length.setter
+    def length(self, new_len):
+        if new_len <= 0:
+            raise ValueError("Длина должна быть больше 0")
+        self._length = new_len
+
+    @width.setter
+    def width(self, new_width):
+        if new_width <= 0:
+            raise ValueError("Ширина должна быть больше 0")
+        self._width = new_width
+
+    def get_perimeter(self):
+        return 2 * (self._width + self._length)
+
+    def get_area(self):
+        return self._width * self._length
+
+    def __add__(self, other):
+        """
+        сложение прямоугольников, складываются периметры исходных прямоугольников
+        :return: экземпляр класса "Rectangle" полученный после сложения периметров
+        """
+        res = self.get_perimeter() + other.get_perimeter()
+        return Rectangle(res)
+
+    def __sub__(self, other):
+        """
+        вычитание прямоугольников, вычитаются периметры исходных прямоугольников
+        :return: экземпляр класса "Rectangle" полученный после вычитания периметров
+        """
+        res = abs(self.get_perimeter() - other.get_perimeter())
+        return Rectangle(res)
+
+    def __eq__(self, other):  # равно ==
+        return self.get_area() == other.get_area()
+
+    def __ne__(self, other):  # неравно !=
+        return self.get_area() != other.get_area()
+
+    def __gt__(self, other):  # больше >
+        return self.get_area() > other.get_area()
+
+    def __ge__(self, other):  # больше или равно >=
+        return self.get_area() >= other.get_area()
+
+    def __lt__(self, other):  # метод меньше <
+        return self.get_area() < other.get_area()
+
+    def __le__(self, other):  # меньше или равно <=
+        return self.get_area() <= other.get_area()
+
+    def __str__(self):
+        res = f'Прямоугольник со сторонами {self._width} и {self._length}'
+        return res
+
+
+rectangle1 = Rectangle(7, 11)
+
+print(rectangle1)
+rectangle1.width = 11
+print(rectangle1)
+rectangle1.length = -9
+print(rectangle1)
+
+
+"""
+Задание №6
+Изменяем класс прямоугольника.
+Заменяем пару декораторов проверяющих длину и ширину
+на дескриптор с валидацией размера.
+
+"""
+
+class Validator:
+    def __init__(self, min_value, max_value):
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def __set_name__(self, owner, name):
+        self.param_name = "_" + name
+
+    def __get__(self, instance, owner):
+        return getattr(instance, self.param_name)
+
+    def __set__(self, instance, value):
+        if self.min_value is not None and value < self.min_value:
+            raise ValueError(f'{value} меньше, чем {self.min_value}')
+        if self.max_value is not None and value > self.max_value:
+            raise ValueError(f'{value} больше, чем {self.max_value}')
+        setattr(instance, self.param_name, value)
+
+
+class Rectangle:
+    """
+    Класс "Прямоугольник" для выполнения действий с прямоугольниками,
+    позволяет сравнивать прямоугольники по площади,
+    получить площадь и периметр прямоугольников
+    складывать и вычитать прямоугольники
+    """
+    width = Validator(10, 20)  # дескриптор
+    length = Validator(10, 40)  # дескриптор
+
+    def __init__(self, width, length):
+        self.width = width
+        if length == 0:
+            length = width
+        self.length = length
+
+    def get_perimeter(self):
+        return 2 * (self.width + self.length)
+
+    def get_area(self):
+        return self.width * self.length
+
+    def __add__(self, other):
+        """
+        сложение прямоугольников, складываются периметры исходных прямоугольников
+        :return: экземпляр класса "Rectangle" полученный после сложения периметров
+        """
+        res = self.get_perimeter() + other.get_perimeter()
+        return Rectangle(res)
+
+    def __sub__(self, other):
+        """
+        вычитание прямоугольников, вычитаются периметры исходных прямоугольников
+        :return: экземпляр класса "Rectangle" полученный после вычитания периметров
+        """
+        res = abs(self.get_perimeter() - other.get_perimeter())
+        return Rectangle(res)
+
+    def __eq__(self, other):  # равно ==
+        return self.get_area() == other.get_area()
+
+    def __ne__(self, other):  # неравно !=
+        return self.get_area() != other.get_area()
+
+    def __gt__(self, other):  # больше >
+        return self.get_area() > other.get_area()
+
+    def __ge__(self, other):  # больше или равно >=
+        return self.get_area() >= other.get_area()
+
+    def __lt__(self, other):  # метод меньше <
+        return self.get_area() < other.get_area()
+
+    def __le__(self, other):  # меньше или равно <=
+        return self.get_area() <= other.get_area()
+
+    def __str__(self):
+        res = f'Прямоугольник со сторонами {self.width} и {self.length}'
+        return res
+
+
+rectangle1 = Rectangle(17, 35)
+
+print(rectangle1)
+rectangle1.width = 15
+rectangle1.length = 40
+print(rectangle1)
---
+++ teory/teory_12.py
@@ -0,0 +1,201 @@
+# Метод вызова функции __call__
+# Создадим класс, экземпляры которого можно вызывать. Например для добавления
+# очередного элемента во внутренний словарь класса по типам.
+from collections import defaultdict
+class Storage:
+    def __init__(self):
+        self.storage = defaultdict(list)
+    def __str__(self):
+        txt = '\n'.join((f'{k}: {v}' for k, v in
+        self.storage.items()))
+        return f'Объекты хранилища по типам:\n{txt}'
+    def __call__(self, value):
+        self.storage[type(value)].append(value)
+        return f'К типу {type(value)} добавлен {value}'
+
+
+# s = Storage()
+# print(s(42))
+# print(s(72))
+# print(s('Hello world!'))
+# print(s(0))
+# print(s)
+
+
+# Возврат очередного значения, __next__
+# Как вы помните из лекции об итераторах и генераторах, любая итерация
+# представляет из себя последовательный вызов функции next() с итератором в
+# качестве аргумента.
+# Для возврата такого значения необходимо определить дандер метод __next__.
+class Fibonacci:
+    def __init__(self, start, stop):
+        self.start = start
+        self.stop = stop
+        self.first = 0
+        self.second = 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while self.first < self.stop:
+            self.first, self.second = self.second, self.first + self.second
+        if self.start <= self.first < self.stop:
+            return self.first
+        raise StopIteration
+
+
+fib = Fibonacci(20, 100)
+for num in fib:
+    print(num)
+
+
+
+import sqlite3
+class DB:
+    def __init__(self, name):
+        self.name = name
+        self.connection = None
+        self.cursor = None
+    def __enter__(self):
+        self.connection = sqlite3.connect(self.name)
+        self.cursor = self.connection.cursor()
+        return self.cursor
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.connection.commit()
+        self.connection.close()
+        self.cursor = self.connection = None
+
+
+db = DB('sqlite.db')
+with db as cur:
+    cur.execute("""create table if not exists users(name,age);""")
+    cur.execute("""insert into users values ('Гвидо', 66);""")
+
+
+#  Декоратор @property
+# На прошлой лекции мы работали с классом треугольник и пометили его свойства
+# защищёнными, добавив символ подчёркивания в начале имени. Но что если доступ
+# к свойству нужен. Хотя бы на чтение. Для этого отлично подойдёт функция
+# декоратор property(). Рассмотрим на более простом и коротком примере.
+class User:
+    def __init__(self, name):
+        self._name = name
+    @property
+    def name(self):
+        return self._name
+
+
+# user = User('Стивен')
+# print(f'{user.name = }')
+# user.name = 'Славик'
+
+
+# Getter
+# Декоратор property позволяет создавать “геттеры”. Это методы, которые выдают
+# себя за свойства, позволяют прочитать результат, но блокируют возможность
+# записи. Рассмотрим другой пример “геттера”.
+class User:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+    @property
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+user = User('Стивен', 'Спилберг')
+print(f'{user.first_name = }\n{user.last_name =}\n{user.full_name = }')
+# user.full_name = 'Стивен Хокинг'
+user.last_name = 'Хокинг'
+print(f'{user.first_name = }\n{user.last_name =}\n{user.full_name = }')
+
+#
+#  Setter
+# Python позволяет к “геттеру” добавить “сеттер” — метод контролирующий
+# изменение свойства. Добавим пользователю возраст и будем контролировать
+# чтобы новый возраст был больше старого. Например мы вручную обновляем
+# данные раз в 5-10 лет.
+class User:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self._age = 0
+
+    @property
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, value):
+        if value > self._age:
+            self._age = value
+        else:
+            raise ValueError(f'Новый возраст должен быть больше текущего: {self._age}')
+
+    @age.deleter
+    def age(self):
+        self._age = 0
+
+
+user = User('Стивен', 'Спилберг')
+user.age = 75
+print(f'Меня зовут {user.full_name} и мне {user.age} лет.')
+print('Прошёл один год.')
+user.age = 76
+print(f'Меня зовут {user.full_name} и мне {user.age} лет.')
+print('Прошло несколько лет. Изобретена технология омоложения. Но возраст она не уменьшает.')
+
+del user.age
+print(f'Меня зовут {user.full_name} и мне {user.age} лет.')
+
+
+#Дескрипторы
+class Range:
+    def __init__(self, min_value: int = None, max_value: int = None):
+        self.min_value = min_value
+        self.max_value = max_value
+    def __set_name__(self, owner, name):
+        self.param_name = '_' + name
+    def __get__(self, instance, owner):
+        return getattr(instance, self.param_name)
+    def __set__(self, instance, value):
+        self.validate(value)
+        setattr(instance, self.param_name, value)
+    def __delete__(self, instance):
+        raise AttributeError(f'Свойство "{self.param_name}" нельзя удалять')
+    def validate(self, value):
+        if not isinstance(value, int):
+            raise TypeError(f'Значение {value} должно быть целым числом')
+        if self.min_value is not None and value < self.min_value:
+            raise ValueError(f'Значение {value} должно быть больше или равно {self.min_value}')
+        if self.max_value is not None and value >= self.max_value:
+            raise ValueError(f'Значение {value} должно быть меньше {self.max_value}')
+
+class Student:
+    age = Range(3, 103)
+    grade = Range(1, 11 + 1)
+    office = Range(3, 42 + 1)
+    def __init__(self, name, age, grade, office):
+        self.name = name
+        self.age = age
+        self.grade = grade
+        self.office = office
+    def __repr__(self):
+        return f'Student(name={self.name}, age={self.age}, grade={self.grade}, office={self.office})'
+
+
+if __name__ == '__main__':
+std_one = Student('Архимед', 12, 4, 29)
+std_other = Student('Аристотель', 2406, 5, 17) # ValueError:Значение 2406 должно быть меньше 103
+print(f'{std_one = }')
+std_one.age = 15
+print(f'{std_one = }')
+std_one.grade = 11.0 # TypeError: Значение 11.0 должно быть целым числом
+std_one.office = 73 # ValueError: Значение 73 должно быть меньше 42
+del std_one.age # AttributeError: Свойство "_age" нельзя удалять
+print(f'{std_one.__dict__ = }')
